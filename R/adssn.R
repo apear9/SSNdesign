c48dd9b81a77af3e17aa7e8bb920ff39849e7e1a
Ben R Fitzpatrick <ben.r.fitzpatrick@gmail.com>
2017-11-07 16:08:28 +1000

    initial commit

---
+++ R/adssn.R
@@ -0,0 +1,12 @@
+#' adssn: Adaptive Design for Spatial Stream Networks
+#'
+#' The adssn package builds upon the SSN package to provide a complete workflow for adaptive sampling design for models of spatial stream networks.
+#'
+#' @section adssn functions:
+#' the adssn package contains the following functions: (list of function names with brief description and link to associated manual pages)
+#'
+#' @docType package
+#' @name adssn
+#' @importClassesFrom SSN SpatialStreamNetwork
+NULL
+## NULL
---
+++ README.md
@@ -0,0 +1,8 @@
+## Adaptive Design for Spatial Stream Networks
+
+### Installation:
+
+Install `adssn` directly from GitHub with `devtools`
+
+```devtools::install_github(repo = 'apear9/adssn')```
+
