efbc7e3bf34bb75dad1c2a68a42574e0e451c72e
Alan R. Pearse <alan.pearse@connect.qut.edu.au>
2018-05-01 08:55:29 +1000

    Replacing/renaming package documentation file
    I've replaced adssn.R with SSNDesign.R to reflect the new package name.

---
+++ R/SSNDesign.R
@@ -0,0 +1,52 @@
+#' SSNDesign: Optimal and adaptive design for Spatial Stream Networks
+#'
+#' The adssn package builds on the SSN package to provide a framework for solving design problems on SpatialStreamNetwork objects.
+#'
+#' @section SSNDesign functions:
+#' the SSNDesign package contains the following functions:
+#' - constructTotalMatrix
+#' - CPOptimality
+#' - doAdaptiveDesign
+#' - doAdaptiveDesignParallel
+#' - DOptimality
+#' - EDOptimality
+#' - EKOptimality
+#' - findOptimalDesign
+#' - findOptimalDesignParallel
+#' - generateSites
+#' - getBIDtables
+#' - getImportantMatrices.obs
+#' - getImportantMatrices.pxo
+#' - getStreamDistMatsInOrder.predsxobs
+#' - getStreamDistMatsInOrder
+#' - glmssn_minimal
+#' - hardCoreDesign
+#' - importStreams
+#' - KOptimality
+#' - ndpoints
+#' - nnetwork
+#' - nppoints
+#' - searchAllDesigns
+#' - sequentialCPOptimality
+#' - sequentialDOptimality
+#' - sequentialEDOptimality
+#' - SimulateOnSSN_minimal
+#' - stableInverse
+#' - systematicDesign
+#'
+#' @docType package
+#' @name SSNDesign
+#' @import foreach
+#' @import iterators
+#' @import itertools
+#' @import doParallel
+#' @import SSN
+#' @import DBI
+#' @import shp2graph
+#' @import maptools
+#' @import igraph
+#' @import rgdal
+#' @import RSQLite
+#' @import stringr
+NULL
+## NULL
