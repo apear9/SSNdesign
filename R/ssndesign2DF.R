884ab11f06715261d28631369b1d5b27ca527d06
Alan R. Pearse <alan.pearse@connect.qut.edu.au>
2018-05-08 19:44:01 +1000

    Update to ssndesign2DF
    Function can now spit out dataframe containing raw data in case histograms, etc., are of interest.

--- R/ssndesign2DF.R
+++ R/ssndesign2DF.R
@@ -6,7 +6,7 @@
 #' 
 #'@usage 
 #' 
-#' \code{ssndesign2DF(ssndesign)}
+#' \code{ssndesign2DF(ssndesign, summarise = TRUE)}
 #' 
 #'@param ssndesign The list object output by one of the main design functions.
 #'@return A data.frame object.
@@ -16,7 +16,7 @@
 #'This function does stuff.
 #'  
 #'@export
-ssndesign2DF <- function(ssndesign){
+ssndesign2DF <- function(ssndesign, summarise = TRUE){
   
   # Check the input
   if(!is.list(ssndesign)){
@@ -47,42 +47,76 @@ ssndesign2DF <- function(ssndesign){
   # Pull into dataframe
   df <- data.frame()
   
-  for(i in 1:n.net){
+  # Two cases: summarise = TRUE -> summary stats. summarise = FALSE -> raw data
+  if(summarise){
     
-    for(j in 1:K){
+    for(i in 1:n.net){
       
-      # Few things to find here
-      # 1. The random start for this kth hyper-iteration
-      # 2. The number of times the while loop ran
-      n.j <- length(ssndesign$utility.values[[i]][[j]]) - 1 # The first value is always a random design
-      n.iter <- n.j/num.evals.per.iter[i]
-      
-      # Begin constructing dataframe
-      df.tmp <- data.frame(
-        min = rep(0, n.iter + 1),
-        median = rep(0, n.iter + 1),
-        max = rep(0, n.iter + 1),
-        K = rep(j, n.iter + 1),
-        itr = 0:n.iter, # Zero is the random start
-        network = rep(i, n.iter + 1)
-      )
-      
-      # Find mins, maxes per iteration
-      df.tmp[1, 1:3] <- ssndesign$utility.values[[i]][[j]][1]
-      
-      for(k in 2:(n.iter + 1)){
-        indices.k <- (k + (k - 2) * num.evals.per.iter[i]):((k - 1) * num.evals.per.iter[i])
-        df.tmp[k, 1] <- min(ssndesign$utility.values[[i]][[j]][indices.k])
-        df.tmp[k, 2] <- median(ssndesign$utility.values[[i]][[j]][indices.k])
-        df.tmp[k, 3] <- max(ssndesign$utility.values[[i]][[j]][indices.k])
+      for(j in 1:K){
+        
+        # Few things to find here
+        # 1. The random start for this kth hyper-iteration
+        # 2. The number of times the while loop ran
+        n.j <- length(ssndesign$utility.values[[i]][[j]]) - 1 # The first value is always a random design
+        n.iter <- n.j/num.evals.per.iter[i]
+        
+        # Begin constructing dataframe
+        df.tmp <- data.frame(
+          min = rep(0, n.iter + 1),
+          median = rep(0, n.iter + 1),
+          max = rep(0, n.iter + 1),
+          K = rep(j, n.iter + 1),
+          itr = 0:n.iter, # Zero is the random start
+          network = rep(i, n.iter + 1)
+        )
+        
+        # Find mins, maxes per iteration
+        df.tmp[1, 1:3] <- ssndesign$utility.values[[i]][[j]][1]
+        
+        for(k in 2:(n.iter + 1)){
+          indices.k <- (k + (k - 2) * num.evals.per.iter[i]):((k - 1) * num.evals.per.iter[i])
+          df.tmp[k, 1] <- min(ssndesign$utility.values[[i]][[j]][indices.k])
+          df.tmp[k, 2] <- median(ssndesign$utility.values[[i]][[j]][indices.k])
+          df.tmp[k, 3] <- max(ssndesign$utility.values[[i]][[j]][indices.k])
+        }
+        
+        df <- rbind(df, df.tmp)
+        
       }
       
-      df <- rbind(df, df.tmp)
+    }
+    
+  } else {
+    
+    for(i in 1:n.net){
+      
+      for(j in 1:K){
+        
+        # Few things to find here
+        # 1. The random start for this kth hyper-iteration
+        # 2. The number of times the while loop ran
+        n.j <- length(ssndesign$utility.values[[i]][[j]]) - 1 # The first value is always a random design
+        n.iter <- n.j/num.evals.per.iter[i]
+        
+        # Begin constructing dataframe
+        df.tmp <- data.frame(
+          utility = ssndesign$utility.values[[i]][[j]],
+          K = rep(j, n.j + 1),
+          itr = c(0, rep(1:n.iter, each = num.evals.per.iter[i])), 
+          network = rep(i, n.j + 1)
+          )
+          
+          df <- rbind(df, df.tmp)
+          
+      }
       
     }
     
   }
   
+  ## Spit out the resulting dataframe
+  
   return(df)
   
 }
+
