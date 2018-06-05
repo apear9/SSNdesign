7de8617c496ef108f35cc38bc89ce2f813a1c72e
Alan R. Pearse <alan.pearse@connect.qut.edu.au>
2018-06-05 14:13:23 +1000

    function to extract data from the stream edges and join that data to the observed or prediction sites
    Needed this as part of the case study. Good thing to have.

---
+++ R/extractStreamEdgeCovariates.R
@@ -0,0 +1,72 @@
+#' Extract covariates on stream edges to observed and/or predicted sites
+#' 
+#'@description 
+#'
+#' This function is designed to get covariates associated with the data slot of the SSN obejct and join them to the point.data slot of the observed and/or predicted sites slot.
+#' 
+#'@usage 
+#' 
+#'\code{extractStreamEdgeCovariates(ssn)}
+#' 
+#'@param ssn an object of class SpatialStreamNetwork
+#'@param columns a vector of column names that should be extracted from the data slot of the SpatialStreamNetwork object
+#'@return An object of class SpatialStreamNetwork. 
+#'
+#'@details
+#'
+#'This function can be useful when the stream edges contain data that you want to associate with the sites for modelling or prediction purposes. An example is watershed attributes for the stream segments.
+#'
+#'@export
+extractStreamEdgeCovariates <- function(ssn, columns){
+  
+  # Number of columns
+  nc <- length(columns)
+  
+  # Check inputs
+  if(nc < 1){
+    stop("You must specify at least one column to extract data from.")
+  }
+  
+  # Check whether prediction sites exist
+  preds.exist <- length(ssn@predpoints@SSNPoints) > 0
+  # Add columns to observed +/- pred points and join
+  ssn@obspoints@SSNPoints[[1]]@point.data$shreve <- 0
+  ssn@obspoints@SSNPoints[[1]]@point.data$afv <- 0
+  obs.unique.rids <- unique(ssn@obspoints@SSNPoints[[1]]@point.data$rid)
+  obs.unique.rids.ind <- match(obs.unique.rids, ssn@data$rid)
+  # Get a list of values in each column
+  col.list <- vector("list", nc)
+  for(i in 1:nc){
+    col.list[[i]] <- ssn@data[obs.unique.rids.ind, columns[i]]
+  }
+  names(col.list) <- columns
+  n.unique.rids <- length(obs.unique.rids)
+  for(i in 1:n.unique.rids){
+    for(j in 1:nc){
+      ind <- which(ssn@obspoints@SSNPoints[[1]]@point.data$rid == obs.unique.rids[i])
+      ssn@obspoints@SSNPoints[[1]]@point.data[ind, columns[[j]]] <- col.list[[j]][i]
+    }
+  }
+  if(preds.exist){
+    ssn@predpoints@SSNPoints[[1]]@point.data$shreve <- 0
+    ssn@predpoints@SSNPoints[[1]]@point.data$afv <- 0
+    pred.unique.rids <- unique(ssn@predpoints@SSNPoints[[1]]@point.data$rid)
+    pred.unique.rids.ind <- match(pred.unique.rids, ssn@data$rid)
+    col.list <- vector("list", nc)
+    for(i in 1:nc){
+      col.list[[i]] <- ssn@data[preds.unique.rids.ind, columns[i]]
+    }
+    names(col.list) <- columns
+    n.unique.rids <- length(preds.unique.rids)
+    for(i in 1:n.unique.rids){
+      for(j in 1:nc){
+        ind <- which(ssn@predspoints@SSNPoints[[1]]@point.data$rid == preds.unique.rids[i])
+        ssn@predspoints@SSNPoints[[1]]@point.data[ind, columns[[j]]] <- col.list[[j]][i]
+      }
+    }
+  }
+  
+  # Return
+  return(ssn)
+  
+}
