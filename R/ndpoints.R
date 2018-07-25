1bbed9966e7581210ed46c580fb15aa795028245
Alan R. Pearse <alan.pearse@connect.qut.edu.au>
2018-07-25 17:55:57 +1000

    Updates to two helper functions
    ndpoints and nppoints are now able to handle temporal replicates. User must specify whether the function should count pids or locIDs.

--- R/ndpoints.R
+++ R/ndpoints.R
@@ -6,9 +6,10 @@
 #'
 #'@usage
 #'
-#'\code{ndpoints(ssn)}
+#'\code{ndpoints(ssn, use = "pids")}
 #'
 #'@param ssn An object of class SpatialStreamNetwork
+#'@param use A string indicating whether pids or locIDs should be used to count sites.
 #'@return A list with two elements: a vector of design points by network, and a scalar being the total number of design points in the SpatialStreamNetwork object.
 #'
 #'@details
@@ -16,28 +17,65 @@
 #'To find the number of prediction points on a SpatialStreamNetwork object, use \code{\link{nppoints}}.
 #' 
 #'@export
-ndpoints <- function(ssn){
+ndpoints <- function(ssn, use = "pids"){
   
-  ## Get vector of networks
+  # Early exits for bad inputs
+  if(!(use %in% c("pids", "locIDs"))){
+    stop("The argument use must be either pids or locIDs. Check spelling.")
+  }
   
-  nets <- ssn@obspoints@SSNPoints[[1]]@point.data$netID
-  nets <- as.numeric(as.character(nets))
-  
-  ## Get unique networks
-  
-  nets.u <- unique(nets)
-  nets.u <- nets.u[order(nets.u)]
-  
-  ## Find number of points
-  
-  ndp <- vapply(nets.u, function(x){sum(nets == x)}, vector("numeric", 1))
-  names(ndp) <- paste("Net", nets.u)
+  if(use == "pids"){
+    ## Get vector of networks
+    
+    nets <- ssn@obspoints@SSNPoints[[1]]@point.data$netID
+    nets <- as.numeric(as.character(nets))
+    
+    ## Get unique networks
+    
+    nets.u <- unique(nets)
+    nets.u <- nets.u[order(nets.u)]
+    
+    ## Find number of points
+    
+    # Per network
+    ndp.net <- vapply(nets.u, function(x){sum(nets == x)}, vector("numeric", 1))
+    names(ndp.net) <- paste("Net", nets.u)
+    
+    # Overall
+    ndp <- sum(ndp.net)
+    
+  } else {
+    
+    ## Get information on unique locations
+    
+    locs.u <- unique(ssn@obspoints@SSNPoints[[1]]@point.data$locID)
+    
+    ## Per network
+    
+    locs.nets.u <- unique(ssn@obspoints@SSNPoints[[1]]@point.data[,c("locID", "netID")])
+    nets.u <- unique(locs.nets.u$netID)
+    nets.n <- length(nets.u)
+    
+    ## Summarise information
+    
+    # Per network
+    
+    ndp.net <- vector("numeric", nets.n)
+    for(i in 1:nets.n){
+      ndp.net[i] <- sum(locs.nets.u$netID == nets.u[i])
+    }
+    names(ndp.net) <- paste("Net", nets.u)
+    
+    # Overall
+    ndp <- length(locs.u)
+    
+  }
   
   ## Return
   
   return(list(
-    byNetwork = ndp,
-    inTotal = sum(ndp)
+    byNetwork = ndp.net,
+    inTotal = ndp
   )
   )
   
--- R/nppoints.R
+++ R/nppoints.R
@@ -6,9 +6,10 @@
 #'
 #'@usage
 #'
-#'\code{nppoints(ssn)}
+#'\code{nppoints(ssn, use = "pids")}
 #'
 #'@param ssn An object of class SpatialStreamNetwork
+#'@param use A string indicating whether the pids or locIDs should be counted. 
 #'@return A list with two elements: a vector of prediction points by network, and a scalar being the total number of prediction points in the SpatialStreamNetwork object.
 #'
 #'@details
@@ -25,26 +26,63 @@ nppoints <- function(ssn){
     return(0)
   }
   
-  ## Get vector of networks
+  # Early exits for bad inputs
+  if(!(use %in% c("pids", "locIDs"))){
+    stop("The argument use must be either pids or locIDs. Check spelling.")
+  }
   
-  nets <- ssn@predpoints@SSNPoints[[1]]@point.data$netID
-  nets <- as.numeric(as.character(nets))
-  
-  ## Get unique networks
-  
-  nets.u <- unique(nets)
-  nets.u <- nets.u[order(nets.u)]
-  
-  ## Find number of points
-  
-  npp <- vapply(nets.u, function(x){sum(nets == x)}, vector("numeric", 1))
-  names(npp) <- paste("Net", nets.u)
+  if(use == "pids"){
+    ## Get vector of networks
+    
+    nets <- ssn@predpoints@SSNPoints[[1]]@point.data$netID
+    nets <- as.numeric(as.character(nets))
+    
+    ## Get unique networks
+    
+    nets.u <- unique(nets)
+    nets.u <- nets.u[order(nets.u)]
+    
+    ## Find number of points
+    
+    # Per network
+    npp.net <- vapply(nets.u, function(x){sum(nets == x)}, vector("numeric", 1))
+    names(npp.net) <- paste("Net", nets.u)
+    
+    # Overall
+    npp <- sum(npp.net)
+    
+  } else {
+    
+    ## Get information on unique locations
+    
+    locs.u <- unique(ssn@predpoints@SSNPoints[[1]]@point.data$locID)
+    
+    ## Per network
+    
+    locs.nets.u <- unique(ssn@predpoints@SSNPoints[[1]]@point.data[,c("locID", "netID")])
+    nets.u <- unique(locs.nets.u$netID)
+    nets.n <- length(nets.u)
+    
+    ## Summarise information
+    
+    # Per network
+    
+    npp.net <- vector("numeric", nets.n)
+    for(i in 1:nets.n){
+      npp.net[i] <- sum(locs.nets.u$netID == nets.u[i])
+    }
+    names(npp.net) <- paste("Net", nets.u)
+    
+    # Overall
+    npp <- length(locs.u)
+    
+  }
   
   ## Return
   
   return(list(
-    byNetwork = npp,
-    inTotal = sum(npp)
+    byNetwork = npp.net,
+    inTotal = npp
   )
   )
   
