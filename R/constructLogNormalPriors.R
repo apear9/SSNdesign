935175b491c487c5a40a6e6bb5de74bd3aa2af73
Alan R. Pearse <17267197+apear9@users.noreply.github.com>
2019-03-31 13:55:34 +1000

    Rename function, change workings
    Now can extract estimated SEs of cov parameters from glmssn. Also can calculate expected SEs when these are missing. Delta method transformation implemented.

--- R/constructLogNormalPriors.R
+++ R/constructLogNormalPriors.R
@@ -2,13 +2,9 @@
 #' 
 #' @description 
 #' 
-#' The main design function in SSNdesign, \code{optimiseSSNDesign}, has an argument specifying a list of independent priors on the covariance parameters of a fitted glmssn object.
+#' The main design function in \code{SSNdesign} is called \code{optimiseSSNDesign}, and it has an argument specifying a list of independent priors on the covariance parameters of a fitted glmssn object.
 #' 
-#' @usage 
-#' 
-#' \code{constructLogNormalCovPriors(glmssn, std = c(0.25, 0.5))}
-#'
-#' @param glmssn A fitted glmssn object.
+#' @param glmssn A fitted glmssn object, preferably with \code{optimOutput$hessian}.
 #' @param std A numeric vector. This vector specifies the standard deviation of the log-normal priors. See Details for more information.
 #' @return A list whose elements are functions parameterised in terms of x, the number of Monte Carlo draws to be taken from the priors when evaluating the expected utility. 
 #' 
@@ -22,23 +18,35 @@
 #' s <- createSSN(10, binomialDesign(10), path = paste(tempdir(), "s.ssn", sep = "/"), importToR = TRUE)
 #' 
 #' # Simulate data
-#' s <- SimulateOnSSN(s, getSSNdata.frame(s), formula = ~ 1, CorModels = c("Exponential.tailup"), CorParms = c(1, 2, 0.1), addfunccol = "addfunccol")$ssn.object
+#' s <- SimulateOnSSN(s, getSSNdata.frame(s), formula = ~ 1, CorModels = c("Spherical.tailup"), CorParms = c(1, 2, 0.1), addfunccol = "addfunccol")$ssn.object
 #' 
 #' # Fit model
-#' m <- glmssn(s, formula = Sim_Values ~ 1, CorModels = c("Exponential.tailup"), addfunccol = "addfunccol")
+#' m <- glmssn(s, formula = Sim_Values ~ 1, CorModels = c("Spherical.tailup"), addfunccol = "addfunccol")
 #' 
 #' # Construct log-normal priors
-#' p <- constructLogNormalCovPriors(m)
+#' p <- constructLogNormalPriors(m)
 #' 
 #' @export
-constructLogNormalCovPriors <- function(glmssn, std = c(0.25, 0.5)){
+constructLogNormalPriors <- function(glmssn, std){
   
   # Check inputs
   if(class(glmssn) != "glmssn"){
     stop("The argument glmssn must be an object of class glmssn.")
   }
-  if(!is.numeric(std)){
-    stop("The argument std must be numeric.")
+  if(missing(std)){
+    # Can either use observed or expected fisher info
+    if(is.null(glmssn$optimOutput$hessian)){
+      warning("The argument glmssn does not contain glmssn$optimOutput$hessian, so the expected fisher information will be used instead.")
+      # theta <- glmssn$estimates$theta[1:length(glmssn$estimates$theta)]
+      std <- theta.se <- getExpectedSE(glmssn, log.scale = TRUE)
+      # std <- log.theta.se <- (1/theta) * theta.se
+    } else {
+      # Use delta method to find the se of the log(thetas)
+      # inv.hessian <- solve(glmssn$optimOutput$hessian)
+      # theta <- glmssn$estimates$theta[1:length(glmssn$estimates$theta)]
+      # theta.se <- sqrt(diag(inv.hessian))
+      std <- theta.se <- getCalculatedSE(glmssn, log.scale = TRUE)
+    }
   }
   
   # extract out estimates of covariance parameters
